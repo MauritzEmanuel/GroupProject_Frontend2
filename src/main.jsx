0331b60ec35dca6dfd5f18791a51a2ebdef9e288
hanna98 <hanna.marohn@gmail.com>
2024-01-23 14:10:27 +0100

    Importerade reactRouter

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,26 @@ import {
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/', //Såhär ser det ut för att komma till hemsidan
     element: <div>Hejsan allihopa :D</div>
   }
 ])
+
+/*
+Skrivs såhär i navigationsbaren/headern:
+<nav>
+  <ul>
+    <li>
+      <a href={`/contacts/1`}>Your Name</a>
+      //För att komma till t.ex kontaktsidan från hemsidan
+      //för att kunna stanna kvar på våran egna webbplats
+    </li>
+    <li>
+      <a href={`/contacts/2`}>Your Friend</a>
+    </li>
+  </ul>
+</nav>
+*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
