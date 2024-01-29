22c34e9934446ea62cb109963ddf0477d70f6970
Lara Pirdaoud <lara.pirdaoud@hotmail.com>
2024-01-29 23:44:56 +0100

    Footer updater

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import AboutUs from './pages/AboutUs.jsx'
 import Product from './pages/Products.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
-import Footer2 from './components/Footer2.jsx'
+import Subscribe from './components/Subscribe.jsx'
 
 
 const router = createBrowserRouter([
@@ -33,6 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
     <Navbar/>
     <RouterProvider router={router}/>
+    <Subscribe/>
     <Footer/>
   </React.StrictMode>,
 )
