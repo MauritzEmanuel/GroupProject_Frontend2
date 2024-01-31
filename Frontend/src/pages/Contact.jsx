a8d19a8058d5007e533da845a001bf9489470a64
Baschir Sheikh Abdi <baschir.sheikhabdi@yh.nackademin.se>
2024-01-31 11:10:47 +0100

    test3

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,11 +1,37 @@
+import React from 'react';
+import '../cssFiles/contact.css';
+
 const Contact = () => {
+  return (
+    <div className='Box'>
+      <div className='form-group'>
+        <input type='text' id='firstName' name='firstName' placeholder=' ' />
+        <label htmlFor='firstName'>First Name:</label>
+      </div>
 
-    return (
-        
-        <div>
-            <h1>En kontakt sida</h1>
-        </div>
-    )
-}
+      <div className='form-group'>
+        <input type='text' id='lastName' name='lastName' placeholder=' ' />
+        <label htmlFor='lastName'>Last Name:</label>
+      </div>
 
-export default Contact
+      <div className='form-group'>
+        <input type='email' id='email' name='email' placeholder=' ' />
+        <label htmlFor='email'>Email:</label>
+      </div>
+
+      <div className='form-group'>
+        <input type='tel' id='phone' name='phone' placeholder=' ' />
+        <label htmlFor='phone'>Phone Number:</label>
+      </div>
+
+      <div className='form-group'>
+        <textarea id='message' name='message' rows='4' placeholder=' '></textarea>
+        <label htmlFor='message'>Message:</label>
+      </div>
+
+      <button type='submit'>Submit</button>
+    </div>
+  );
+};
+
+export default Contact;
