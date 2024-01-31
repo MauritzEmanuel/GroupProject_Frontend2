8eb98279c3e5e2f893502bb345e0de2bfbaee351
MauritzEmanuel <emanuel.lundbergh@yh.nackademin.se>
2024-01-31 12:39:54 +0100

    hämta produkter mall

--- Frontend/src/components/ProductlistItem.jsx
+++ Frontend/src/components/ProductlistItem.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React,  from "react";
 
 const ProductListItem = ({ product }) => {
-    <li key={product.id}>
-        <h2>{product.Title}</h2>
-        <h3>{product.Auther}</h3>
-        <p>{product.Image}</p>
-        <p>{product.Description}</p>
-        <p>Pris: {product.price} KR</p>
-    </li>
+    return(
+        <li key={product.id}>
+            <h2>{product.Title}</h2>
+            <h3>{product.Author}</h3>
+            <p>{product.Image}</p>
+            <p>{product.Description}</p>
+            <p>Pris: {product.price} KR</p>
+        </li>
+    )
 }
 
 export default ProductListItem
